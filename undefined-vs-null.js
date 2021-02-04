f738dd4ecc75d070c059c1718cdeeda55c66b769
StarsWarrior <waliullahshuvo6@gmail.com>
2021-02-04 21:07:25 +0600

    null vs undefined xleared for interview

---
+++ undefined-vs-null.js
@@ -0,0 +1,19 @@
+// let name;
+
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return
+// }
+
+// function add(num1, num2){
+//     console.log(num1, num2);
+// }
+
+// const student = {name:"shuvo", phone:01303118600};
+// console.log(student.gf);
+
+// let fun = undefined;
+// console.log(fun);
+
+let ages = [2, 5, 6, 9]
+console.log(ages[15]);
