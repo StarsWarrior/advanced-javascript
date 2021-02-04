15fa206d0f98dc331fe22a583bd1a8716600c51d
StarsWarrior <waliullahshuvo6@gmail.com>
2021-02-04 20:53:40 +0600

    this is true vs false

---
+++ true-false.js
@@ -0,0 +1,17 @@
+// const name = "0";
+//0
+//""
+//undefined
+//null
+// '0'
+// Truthy
+// '0', ' ' []
+let name = 12;
+console.log(name);
+
+if(name || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
