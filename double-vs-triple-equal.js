4c601a920cf1918007edaffdeee0924d4b558467
StarsWarrior <waliullahshuvo6@gmail.com>
2021-02-04 21:18:04 +0600

    doubble and tripple Equals Sign Using concepts are cleared for interview

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,11 @@
+// == (double equals for checking just value)
+// & === (triple equals for checking type and value both)
+
+const first = 0;
+const second = false;
+if(first == second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
